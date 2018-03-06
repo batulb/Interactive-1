323d03d4b4d44a25c8bfd066ad43a2f6cd763080
Batul <batulb@github.com>
2018-03-05 21:10:41 -0800

    vgjvj

--- Project-2/Coding/assets/js/script.js
+++ Project-2/Coding/assets/js/script.js
@@ -4,3 +4,31 @@ $(document).ready(function(){
     $(this).next('.item').addClass('show');
 	});
 });
+
+$(document).ready(function(){
+
+
+	// click to start the entire sequence
+	$('.item').click(function(){
+
+	  	// loop through each element with the class 'item' in order
+		$('.item').each(function(i) {
+
+			// set the current element with class 'item' as a variable
+			var item = $(this);
+
+			// every x seconds, do this function
+			setTimeout( function(){
+
+				// use the item variable, or the current 'item'
+				$(item).removeClass('show');
+				$(item).next('.item').addClass('show');
+
+			// adjust 5000 to make it go faster or slower
+    }, i * 6000);
+
+		});
+
+	});
+
+});
