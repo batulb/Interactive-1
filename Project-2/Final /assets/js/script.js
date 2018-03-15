6ef16f053db2c13dade7e867c4c4b21a9caae68e
Batul <batulb@github.com>
2018-03-14 21:30:21 -0700

    vhhkvj

--- Project-2/Final /assets/js/script.js
+++ Project-2/Final /assets/js/script.js
@@ -1,30 +1,44 @@
 $(document).ready(function(){
   //
-  $('.item').click(function() {
-    $(this).removeClass('show');
-    $(this).next('.item').addClass('show');
-	});
+  // $('.item').click(function() {
+  //   $(this).removeClass('show');
+  //   $(this).next('.item').addClass('show');
+	// });
 
+  $('.refresh').click(function(){
+    $('#impatient').show();
+    $('#progress').hide();
 
-// var per = 3;
-//   // loop through each element with the class 'item' in order
-// $('.item').each(function(i) {
-//
-//   // set the current element with class 'item' as a variable
-//   var item = $(this);
-//
-//   // every x seconds, do this function
-//   setTimeout( function(){
-//
-//     // use the item variable, or the current 'item'
-//     $(item).removeClass('show');
-//     $(item).next('.item').addClass('show');
-//     per = per + 3;
-//     $('#percent').html(per);
-//   // adjust 5000 to make it go faster or slower
-//   }, i * 6000);
-//
-// });
+    $('.final').html(per);
+
+  });
+
+  var per = 3;
+  var n = 1;
+
+  // loop through each element with the class 'item' in order
+
+  $('.item').each(function(i) {
+
+    // set the current element with class 'item' as a variable
+    var item = $(this);
+
+    if (n < 31) {
+      console.log(n);
+      // every x seconds, do this function
+      setTimeout( function(){
+
+        // use the item variable, or the current 'item'
+        $(item).removeClass('show');
+        $(item).next('.item').addClass('show');
+        per = per + 3;
+        // $('.percent').html(per);
+        // adjust 5000 to make it go faster or slower
+      }, i * 25000);
+    }
+
+    n++;
+  });
 
   // var down = 0;
   // // every x seconds, do this function
