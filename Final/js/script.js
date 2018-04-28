49d1789613eee987d1161add5820938328dbd4cc
Batul <batulb@github.com>
2018-04-28 09:39:13 -0700

    rfdsm

--- Final/js/script.js
+++ Final/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+  //$('.list span').hover(function(){
+    //$('body').addClass('active');
+  //})
+
+  $(".list span").hover(function(){
+        $(this).addClass("active");
+        //}, function(){
+        //$(this).attr('src', 'ImageColl/'+path+'/''background.png');
+    });
+
 	$('span').click(function() {
 	  var path = $(this).data('hastag');
 
@@ -7,11 +17,18 @@ $(document).ready(function(){
     $('#grid img').each(function(){
 
 
-      var number = Math.floor(Math.random()*18);
+      var number = Math.floor(Math.random()*17) + 1;
 
       $(this).attr('src', 'ImageColl/'+path+'/'+number+'.png');
 
     });
   });
+
+  $('body').click(function() {
+     //alert('Are you sure? It is permanent');
+     var y = window.prompt("How would you feel if someone posted the same photograph as you?")
+     // window.alert(y)
+     $('.cover').html(y);
+    });
 
 });
